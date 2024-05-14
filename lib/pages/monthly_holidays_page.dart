bccf8870995dbcc79a2f4b79fe13984f84875a21
Khusanboy Tursunov <tursunovkhusanboy@gmail.com>
2024-05-14 06:49:30 +0500

    completed monthly holidays page

--- lib/pages/monthly_holidays_page.dart
+++ lib/pages/monthly_holidays_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -19,8 +21,8 @@ class MonthlyHolidaysPage extends StatelessWidget {
               child: Stack(
                 children: [
                   Positioned(
-                    top: 80,
-                    left: 50,
+                    top: 70,
+                    left: 30,
                     child: Container(
                       width: 110,
                       height: 110,
@@ -33,8 +35,8 @@ class MonthlyHolidaysPage extends StatelessWidget {
                     ),
                   ),
                   const Positioned(
-                    top: 90,
-                    left: 185,
+                    top: 80,
+                    left: 165,
                     child: Text(
                       "Sara",
                       style: TextStyle(
@@ -45,8 +47,8 @@ class MonthlyHolidaysPage extends StatelessWidget {
                     ),
                   ),
                   const Positioned(
-                    top: 125,
-                    left: 185,
+                    top: 115,
+                    left: 165,
                     child: Text(
                       "Party organizer",
                       style: TextStyle(
@@ -57,14 +59,14 @@ class MonthlyHolidaysPage extends StatelessWidget {
                     ),
                   ),
                   Positioned(
-                    top: 150,
-                    left: 185,
+                    top: 140,
+                    left: 165,
                     child: SizedBox(
                       width: 110,
                       height: 30,
                       child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFFE17855),
+                          backgroundColor: const Color(0xFFE17855),
                         ),
                         onPressed: () {},
                         child: const Text(
@@ -83,13 +85,387 @@ class MonthlyHolidaysPage extends StatelessWidget {
             alignment: Alignment.bottomCenter,
             child: Container(
               width: screenWidth,
-              height: screenHeight * 0.7,
+              height: screenHeight * 0.75,
               decoration: const BoxDecoration(
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(30),
                   topRight: Radius.circular(30),
                 ),
                 color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      text: const TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'October',
+                            style: TextStyle(
+                              fontSize: 26,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black87,
+                            ),
+                          ),
+                          TextSpan(
+                            text: ' Holidays',
+                            style: TextStyle(
+                              fontSize: 26,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.black54,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Stack(
+                              clipBehavior: Clip.none,
+                              children: [
+                                Container(
+                                  width: 60,
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(18),
+                                    color: const Color(0xFFFAEBC8),
+                                  ),
+                                ),
+                                Positioned(
+                                  left: -10,
+                                  top: 10,
+                                  child: Image.asset(
+                                    "assets/images/image.png",
+                                    width: 60,
+                                  ),
+                                )
+                              ],
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Thanksgiving",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w200,
+                                  ),
+                                ),
+                                Text(
+                                  "\$ 174.99",
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 75,
+                          height: 30,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFFD7E1C8),
+                            ),
+                            onPressed: () {},
+                            child: const Text(
+                              'View',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Divider(),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Stack(
+                              clipBehavior: Clip.none,
+                              children: [
+                                Container(
+                                  width: 60,
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(18),
+                                    color: const Color(0xFFFAEBC8),
+                                  ),
+                                ),
+                                Positioned(
+                                  left: -10,
+                                  top: 10,
+                                  child: Image.asset(
+                                    "assets/images/image.png",
+                                    width: 60,
+                                  ),
+                                )
+                              ],
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Thanksgiving",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w200,
+                                  ),
+                                ),
+                                Text(
+                                  "\$ 326.00",
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 75,
+                          height: 30,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFFD7E1C8),
+                            ),
+                            onPressed: () {},
+                            child: const Text(
+                              'View',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Divider(),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Stack(
+                              clipBehavior: Clip.none,
+                              children: [
+                                Container(
+                                  width: 60,
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(18),
+                                    color: const Color(0xFFFAEBC8),
+                                  ),
+                                ),
+                                Positioned(
+                                  left: -10,
+                                  top: 10,
+                                  child: Image.asset(
+                                    "assets/images/image.png",
+                                    width: 60,
+                                  ),
+                                )
+                              ],
+                            ),
+                            const SizedBox(
+                              width: 20,
+                            ),
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Thanksgiving",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w200,
+                                  ),
+                                ),
+                                Text(
+                                  "\$ 51.00",
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 75,
+                          height: 30,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFFD7E1C8),
+                            ),
+                            onPressed: () {},
+                            child: const Text(
+                              'View',
+                              style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Divider(),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    RichText(
+                      text: const TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'Party',
+                            style: TextStyle(
+                              fontSize: 26,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black87,
+                            ),
+                          ),
+                          TextSpan(
+                            text: ' Planning',
+                            style: TextStyle(
+                              fontSize: 26,
+                              fontWeight: FontWeight.w300,
+                              color: Colors.black54,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          Column(
+                            children: [
+                              Container(
+                                width: 120,
+                                height: 120,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  image: const DecorationImage(
+                                    image:
+                                        AssetImage("assets/images/autumn.jpg"),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              const Text(
+                                "Birthday",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            children: [
+                              Container(
+                                width: 120,
+                                height: 120,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  image: const DecorationImage(
+                                    image: AssetImage(
+                                        "assets/images/umbrella.jpg"),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              const Text(
+                                "Birthday",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            children: [
+                              Container(
+                                width: 120,
+                                height: 120,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  image: const DecorationImage(
+                                    image:
+                                        AssetImage("assets/images/party.jpg"),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              const Text(
+                                "Birthday",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           )
